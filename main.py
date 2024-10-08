c4a88a30f727d9c4a78e9c525c177b9b042a2b3d
Muhammad Abdulloh <muhammad.abdulloh@kuelap.io>
2024-10-08 18:08:32 +0700

    adding delete cookies

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ target = args.target + '/review'
 print(target)
 driver = uc.Chrome(use_subprocess=False)
 driver.get(target)
-# print(page_handler.get_comments(driver))
+driver.delete_all_cookies()
 print(page_handler.get_all_comments(driver))
 
 # result = text_handler.get_tag("Ibu pergi ke pasar yang jauh di hari yang panas.........")
